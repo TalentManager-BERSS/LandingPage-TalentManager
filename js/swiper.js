7c794bd00a6f992d9fd26f7e09c1dc9a5121da84
luissssrr <u202311334@upc.edu.pe>
2025-04-27 00:45:03 -0500

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,1 +1,18 @@
-
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    autoplay: {
+      delay: 10000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoHeight: true,
+  });
